db30440e59d9fe44fd3ed5f7a6aceebc50016b72
MK <thedeadgamer101@gmail.com>
2022-03-11 17:35:59 +0200

    As an app developer I want to get a list of all criminals to show in my app [Delivers #181539321]
    Create README.md
    
    Update README.md
    
    front_end
    
    Update README.md

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 6661;
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
+
+const criminals = [
+    { id: 1, name: 'Vin Diesel', crime: 'Speeding' },
+    { id: 1, name: 'Henry', crime: 'Racism' },
+    { id: 1, name: 'Jason Voorhees', crime: 'Murder' },
+]
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/criminals', (req, res) => {
+    res.send(criminals)
+})
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
