9bbb8762d3edaeceba9d045a61f071ae766b138c
MK <thedeadgamer101@gmail.com>
2022-03-18 14:10:23 +0200

    As a user I want to see the details of the criminals [#181537921]

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
 
 const criminals = [
-    { id: 1, name: 'Vin Diesel', crime: 'Speeding' },
-    { id: 1, name: 'Henry', crime: 'Racism' },
-    { id: 1, name: 'Jason Voorhees', crime: 'Murder' },
+    { id: 1, name: 'Vin Diesel', crime: 'Speeding', img_link: 'henry.jpg', long_desc: 'Wanted for speeding countlesly in the fast and furious series.' },
+    { id: 2, name: 'Henry', crime: 'Racism', img_link: 'vin-diesel.jpg', long_desc: 'Henry is a natural racist' },
+    { id: 3, name: 'Jason Voorhees', crime: 'Murder', img_link: 'jason-voorhees.jpg', long_desc: "Jason Voorhees is a third-degree murderer. He's prime spot for killing is a camping site." },
 ]
 
 app.use(cors())
@@ -17,6 +17,10 @@ app.use(express.json())
 app.get('/criminals', (req, res) => {
     res.send(criminals)
 })
+
+app.get('/criminals/:id', (req, res) => {
+    res.send(criminals[req.params.id - 1])
+});
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
